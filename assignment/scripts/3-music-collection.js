d418f7b3da86d5a0108a7ed814d117077869aa88
Anissa Crawford <anissacrawford@gmail.com>
2022-02-16 12:13:00 -0600

    changed objects from many to one

--- assignment/scripts/3-music-collection.js
+++ assignment/scripts/3-music-collection.js
@@ -3,43 +3,21 @@ console.log('***** Music Collection *****')
 let collection = [];
 
 function addToCollection (title, artist, yearPublished){
-
-
-  return object;
+  let album = {
+    title: title,
+    artist: artist,
+    yearPublished: yearPublished
+  }
+  return album;
 }
 
-let albumOne = {
-  title: 'AM',
-  artist: 'Artic Monkeys',
-  yearPublished: 2013
-};
+addToCollection();
 
-let albumTwo = {
-  title:
-  artist:
-  yearPublished:
-}
+console.log(addToCollection('AM', 'Arctic Monkeys', 2013));
+console.log(addToCollection('Blonde', 'Frank Ocean', 2016));
+console.log(addToCollection('Circles', 'Mac Miller', 2020));
+console.log(addToCollection('Man On The Moon', 'Kid Cudi', 2009));
+console.log(addToCollection('Social Cues', 'Cage the Elephant', 2019));
+console.log(addToCollection('Purple Rain', 'Prince', 1984));
 
-let albumThree = {
-  title:
-  artist:
-  yearPublished:
-}
-
-let albumFour = {
-  title:
-  artist:
-  yearPublished:
-}
-
-let albumFive = {
-  title:
-  artist:
-  yearPublished:
-}
-
-let albumSix = {
-  title: 
-  artist:
-  yearPublished:
-}
+console.log(collection);
