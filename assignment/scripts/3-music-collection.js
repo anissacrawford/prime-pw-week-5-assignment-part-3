809a9993373b48f6d30e61a5913cbf4e39b0dd39
Anissa Crawford <anissacrawford@gmail.com>
2022-02-16 10:51:39 -0600

    added albums

--- assignment/scripts/3-music-collection.js
+++ assignment/scripts/3-music-collection.js
@@ -1,1 +1,45 @@
 console.log('***** Music Collection *****')
+
+let collection = [];
+
+function addToCollection (title, artist, yearPublished){
+
+
+  return object;
+}
+
+let albumOne = {
+  title: 'AM',
+  artist: 'Artic Monkeys',
+  yearPublished: 2013
+};
+
+let albumTwo = {
+  title:
+  artist:
+  yearPublished:
+}
+
+let albumThree = {
+  title:
+  artist:
+  yearPublished:
+}
+
+let albumFour = {
+  title:
+  artist:
+  yearPublished:
+}
+
+let albumFive = {
+  title:
+  artist:
+  yearPublished:
+}
+
+let albumSix = {
+  title: 
+  artist:
+  yearPublished:
+}
