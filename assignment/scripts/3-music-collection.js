66853c3e5df87b9b1bbf5fc216c840e19069c325
Anissa Crawford <anissacrawford@gmail.com>
2022-02-18 13:25:16 -0600

    got showCollection function working

--- assignment/scripts/3-music-collection.js
+++ assignment/scripts/3-music-collection.js
@@ -10,7 +10,7 @@ function addToCollection (title, artist, yearPublished){
 }
   collection.push(album);
   return album;
-}
+} //Adds albums to collection array
 
 console.log(addToCollection('AM', 'Arctic Monkeys', 2013));
 console.log(addToCollection('Blonde', 'Frank Ocean', 2016));
@@ -18,31 +18,41 @@ console.log(addToCollection('Circles', 'Mac Miller', 2020));
 console.log(addToCollection('Man On The Moon', 'Kid Cudi', 2009));
 console.log(addToCollection('Social Cues', 'Cage the Elephant', 2019));
 console.log(addToCollection('Purple Rain', 'Prince', 1984));
+//calling albums to addToCollection function
 
 console.log(collection);
-
+//logging collection array
 
 function showCollection (array){
   console.log(array.length);
   for (let i=0; i<array.length; i++){
-    console.log(album.title, " by ", album.artist, " published in ", album.yearPublished)
+    console.log(array[i].title, " by ", array[i].artist, " published in ", array[i].yearPublished); //accessing collection[i]
   }
+  return;
 }
 
-console.log(showCollection(collection));
-
-// for (i=0; i<array.length; i++){
-//   console.log(title + 'by' + artist + 'published in' + yearPublished)
-// }
-
+showCollection(collection); //adding collection array to showCollection function and logging
 
 
 // function findByArtist (artist){
 //   let foundArtist = [];
-//   for (collection){
-//     foundArtist.push(artist)
-//   }
+//   //console.log(foundArtist);
+//   for (let i=0; i<collection.length; i++){
+//     if (artist === collection) {
+//     foundArtist = collection.push(artist);
+//     }
+//   } return foundArtist;
 // }
-
+//
 // console.log(findByArtist('Arctic Monkeys'));
 // console.log(findByArtist('Beyonce'));
+
+
+
+
+
+
+function findByArtist (artist) {
+  letFoundArtist = [];
+
+}
